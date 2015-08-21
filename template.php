4ac817a84024f1c8895f1a66dc027a11dc87be76
Oskar Schöldström <public@oxy.fi>
2015-08-21 18:40:43 -0500

    cleanup

--- template.php
+++ template.php
@@ -1,7 +1,7 @@
 <?php
 
 $path = drupal_get_path('theme', 'generoi');
-require_once($path . '/inc/helpers.inc');
+require_once($path . '/inc/process.inc');
 require_once($path . '/inc/preprocess.inc');
 require_once($path . '/inc/theme.inc');
 require_once($path . '/inc/alter.inc');
