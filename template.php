77195b7b4b8d571475c2581c5f01de00c5a106ac
Oskar Schöldström <public@oxy.fi>
2015-08-30 17:02:38 -0500

    facetapi: style facets

--- template.php
+++ template.php
@@ -4,4 +4,5 @@ $path = drupal_get_path('theme', 'generoi');
 require_once($path . '/inc/process.inc');
 require_once($path . '/inc/preprocess.inc');
 require_once($path . '/inc/theme.inc');
+require_once($path . '/inc/theme.facetapi.inc');
 require_once($path . '/inc/alter.inc');
